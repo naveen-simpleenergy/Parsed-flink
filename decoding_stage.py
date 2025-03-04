4464131511b9f784b633aa60904127918a04a6f9
Naveen <naveen-simpleenergy>
2025-03-04 21:06:29 +0530

    parsing logic integrated

--- decoding_stage.py
+++ decoding_stage.py
@@ -11,7 +11,11 @@ from pyflink.common import Configuration
 import json
 from dotenv import load_dotenv
 load_dotenv()
+import cantools
 
+### import dbc
+DBC_FILE_PATH = './dbc_files/SimpleOneGen1_V2_2.dbc'
+dbc_load = cantools.database.load_file(DBC_FILE_PATH) 
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER")
 KAFKA_USERNAME = os.getenv("KAFKA_USERNAME")
@@ -53,34 +57,42 @@ kafka_source = KafkaSource.builder() \
 
 print(f"Kafka source setup complete")
 
-class LimitedPrintMap(object):
-    def __init__(self):
-        self.counter = 0
+class ProcessMap(object):
 
-    def map(self, value):
-        if self.counter < 1:
-            self.counter += 1
-            return f"Received: {value}"
-        else:
-            return None
+    def __init__(self, dbc_load):
+        self.dbc_load = dbc_load
 
-class HexToIntMap(object):
-    def map(self, value):
+    def map_ghoda(self, value):
         data = json.loads(value)
+        
         hex_can_id = data['raw_can_id']
-        int_can_id = int(hex_can_id, 16)
-        data['raw_can_id'] = int_can_id
-        return json.dumps(data)
+        int_can_id = int(str(hex_can_id), 16) & 0x1FFFFFFF
+
+        decoded_can_ID = dbc_load.get_message_by_frame_id(int_can_id)
+
+        byte_array = bytearray([
+            data.pop('byte1'),
+            data.pop('byte2'),
+            data.pop('byte3'),
+            data.pop('byte4'),
+            data.pop('byte5'),
+            data.pop('byte6'),
+            data.pop('byte7'),
+            data.pop('byte8')
+        ])
+        
+        decoded_signals =  decoded_can_ID.decode(byte_array,decode_choices=False)
+        
+        return json.dumps(decoded_signals)
+
 
 watermark_strategy = WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_millis(5000))
 
 data_stream = env.from_source(source=kafka_source, watermark_strategy = watermark_strategy, source_name="Kafka Source")
 
-# data_stream.map(LimitedPrintMap().map, output_type=Types.STRING()) \
-#     .filter(lambda x: x is not None) \
-#     .print()
-
-data_stream.map(HexToIntMap().map, output_type=Types.STRING()) \
+process_map = ProcessMap(dbc_load)
+data_stream.map(process_map.map_ghoda, output_type=Types.STRING()) \
     .print()
 
-env.execute("Kafka Consumer Job - Hex to Int Conversion")
+
+env.execute("Kafka Consumer Job - Data Processing")
