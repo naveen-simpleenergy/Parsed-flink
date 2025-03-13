c4d79e1ce18734db089129a6b8ce01e9e1660e73
Swastik Mohanty <swastik.mohanty@simpleenergy.in>
2025-03-13 15:36:18 +0530

    complete first iteration of pyflink POC in setup env

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from producer import KafkaDataProducer, KafkaProducer
+from producer import KafkaDataProducer, KafkaSender 
 from stages import CANMessageDecoder, FaultFilter
 from utils import KafkaConfig, MessagePayload, setup_flink_environment
 from pyflink.common.typeinfo import Types
@@ -7,28 +7,15 @@ from pyflink.common import Duration
 from dotenv import load_dotenv
 load_dotenv()
 
-import json
 import os
-import cantools
 import sys
-import logging
-
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 DBC_FILE_PATH = './dbc_files/SimpleOneGen1_V2_2.dbc'
-
-def process_and_log(payload, kafka_producer):
-    result = kafka_producer.send_data(payload)
-    logging.info(f"Processed data for VIN {payload.vin}:")
-    for topic, data in result.items():
-        logging.info(f"  Topic: {topic}")
-        logging.info(f"  Data: {json.dumps(data, indent=2)}")
-    return result
 
 def main():
     env = setup_flink_environment(parallelism=1)
     kafka_source = KafkaConfig.create_kafka_source()
+
     kafka_output_config = {
             'brokers': os.getenv("STAGE_KAFKA_BROKER").split(','),
             'security_protocol': os.getenv("SECURITY_PROTOCOL"),
@@ -36,12 +23,6 @@ def main():
             'sasl_username': os.getenv("STAGE_KAFKA_USERNAME"),
             'sasl_password': os.getenv("STAGE_KAFKA_PASSWORD"),
         }
-    
-    print(kafka_output_config["brokers"])
-
-    print("Kafka source setup complete")
-
-    kafka_producer = KafkaDataProducer(kafka_output_config,"signalTopic.json")
 
     can_decoder = CANMessageDecoder(DBC_FILE_PATH)
     fault_filter = FaultFilter(json_file="signalTopic.json")
@@ -53,9 +34,8 @@ def main():
                         .map(lambda x: MessagePayload(x), output_type=Types.PICKLED_BYTE_ARRAY())  
                         .map(lambda x: can_decoder.execute(x), output_type=Types.PICKLED_BYTE_ARRAY())  
                         .map(lambda x: fault_filter.execute(x), output_type=Types.PICKLED_BYTE_ARRAY())  
-                        .map(lambda x: process_and_log(x, kafka_producer))
-                        )
-
+                        .map(KafkaSender(kafka_output_config, "signalTopic.json"),output_type=Types.STRING()))
+                        
     processed_stream.print() 
 
     env.execute("Flink parser")
