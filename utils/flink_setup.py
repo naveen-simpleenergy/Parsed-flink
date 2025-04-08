a2ffd6ac3783fcdcc10c3ff0b064dc0c2f86b327
Swastik Mohanty <swastik.mohanty@simpleenergy.in>
2025-04-08 18:37:36 +0530

    update checkpoint timeout from 15min to 25min

--- utils/flink_setup.py
+++ utils/flink_setup.py
@@ -12,11 +12,11 @@ def setup_flink_environment():
 
     # Checkpointing settings
     checkpoint_config = env.get_checkpoint_config()
-    checkpoint_config.set_checkpoint_interval(5 * 60 * 1000)  # 5 minutes
-    checkpoint_config.set_checkpoint_timeout(15 * 60 * 1000)  # 10 minutes
+    checkpoint_config.set_checkpoint_interval(5 * 60 * 1000) 
+    checkpoint_config.set_checkpoint_timeout(25 * 60 * 1000)  
     checkpoint_config.set_tolerable_checkpoint_failure_number(5)
     checkpoint_config.set_max_concurrent_checkpoints(1)
-    checkpoint_config.set_min_pause_between_checkpoints(60 * 1000)  # 1 minute
+    checkpoint_config.set_min_pause_between_checkpoints(60 * 1000)
     checkpoint_config.enable_unaligned_checkpoints(True)
 
     print("Flink environment setup complete")
